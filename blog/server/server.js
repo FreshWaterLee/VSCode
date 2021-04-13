f2e6da2f35b04a96bfe93b054e6b806f5634ce73
Lee <ihg9303@gmail.com>
2021-04-13 15:56:26 +0900

    "aaaa"

--- blog/server/server.js
+++ blog/server/server.js
@@ -1,55 +1,20 @@
-// const express = require("express"); 
-// const app = express();
-// const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const mysql = require("mysql"); // mysql 모듈 사용
-
-// var connection = mysql.createConnection({
-//     host : "localhost",
-//     user : "lee", //mysql의 id
-//     password : "12345", //mysql의 password
-//     database : "study_db", //사용할 데이터베이스
-// });
-
-// connection.connect();
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// app.get('/', (req, res) =>{
-//     res.send('혁이는 코딩 중!')
-// })
-
-// app.post("/idplz", (req,res)=>{
-//     const test = req.body.test;
-//     // console.log(req.body);
-//     connection.query("INSERT INTO test (name) values (?)",[test],
-//     function(err,rows,fields){
-//         if(err){
-//             console.log("실패");
-//             // console.log(err);
-//         }else{
-//             console.log("성공");
-//             // console.log(rows);
-//         };
-//     });
-
-    
-// });
-
-// app.listen(port, ()=>{
-//     console.log(`Connect at http://localhost:${port}`);
-// })
-
 const express = require("express"); 
 const app = express();
 const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mysql = require("mysql"); // mysql 모듈 사용
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "lee", //mysql의 id
+    password : "12345", //mysql의 password
+    database : "study_db", //사용할 데이터베이스
+});
+
+connection.connect();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -58,8 +23,20 @@ app.get('/', (req, res) =>{
 })
 
 app.post("/idplz", (req,res)=>{
-    const serverid = req.body.plzid;
-    console.log(serverid);
+    const test = req.body.test;
+    // console.log(req.body);
+    connection.query("INSERT INTO test(name) values (?)",[test],
+    function(err,rows,fields){
+        if(err){
+            console.log("실패");
+            // console.log(err);
+        }else{
+            console.log("성공");
+            // console.log(rows);
+        };
+    });
+
+    
 });
 
 app.listen(port, ()=>{
--- blog/src/App.js
+++ blog/src/App.js
@@ -1,78 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h2>Let's develop management System</h2>
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import React, { Component } from 'react'
-// import './App.css';
-// export default class App extends Component {
-//   state = {
-//     testbody : "",
-//   }
-
-//   handleChange =(e)=>{
-//     this.setState({
-//       [e.target.name] : e.target.value,
-//     });
-//   }
-
-//   submitId = ()=>{
-//     const post ={
-//       test : this.state.testbody,
-//     };
-   
-//     fetch("http://localhost:3001/idplz", {
-//       method : "post", // 통신방법
-//       headers : {
-//         "content-type" : "application/json",
-//       },
-//       body : JSON.stringify(post),
-//     })
-//     .then((res)=>res.json())
-//     .then((json)=>{
-//       this.setState({
-//         testbody : json.text,
-//       });
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <input onChange={this.handleChange} name ="testbody"/>
-//         <button onClick = {this.submitId}>Submit</button>
-//         <h1>{this.state.testbody}</h1>
-//       </div>
-//     )
-//   }
-// }
-
 import React, { Component } from 'react'
 import './App.css';
 export default class App extends Component {
   state = {
-    id : "",
+    testbody : "",
   }
 
   handleChange =(e)=>{
@@ -83,7 +13,7 @@ export default class App extends Component {
 
   submitId = ()=>{
     const post ={
-      plzid : this.state.id,
+      test : this.state.testbody,
     };
    
     fetch("http://localhost:3001/idplz", {
@@ -92,15 +22,21 @@ export default class App extends Component {
         "content-type" : "application/json",
       },
       body : JSON.stringify(post),
+    })
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        testbody : json.text,
+      });
     });
   };
 
   render() {
     return (
       <div>
-        <input onChange={this.handleChange} name ="id"/>
+        <input onChange={this.handleChange} name ="testbody"/>
         <button onClick = {this.submitId}>Submit</button>
-        <h1>{this.state.id}</h1>
+        <h1>{this.state.testbody}</h1>
       </div>
     )
   }
