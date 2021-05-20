8827b2115f283c7798b0bf990e854cc428700cf8
Lee <ihg9303@gmail.com>
2021-05-20 13:14:21 +0900

    "05201300Fire"

---
+++ firereac/src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import SigendInLink from './SignedInLink'
+import SigendOutLink from './SignedOutLink'
+
+
+const Navar =()=>{
+    return(
+        <nav className = "nav-wrapper grey darken-3">
+            <div className = "container">
+                <Link to = '/' className="brand-logo"> MarioPlan</Link>
+                <SigendInLink/>
+                <SigendOutLink/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navar;
