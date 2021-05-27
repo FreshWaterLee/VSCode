98ec62919b591f3cc37ed3472610e6fe1f95071b
Lee <ihg9303@gmail.com>
2021-05-27 16:27:48 +0900

    'FireGram_Final'

--- firegram/src/comps/ImageGrid.js
+++ firegram/src/comps/ImageGrid.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import useFirestore from '../hooks/useFirestore'
+import {motion} from 'framer-motion'
 
 const ImageGrid = ({setSelectedImg})=>{
     const {docs} = useFirestore('images');
-    console.log(docs);
     return(
         <div className="img-grid">
             {docs && docs.map(doc =>(
-                <div className="img-wrap" key={doc.id} onClick= {()=>setSelectedImg(doc.durl)}>
-                    <img src={doc.durl} alt ='uploaded pic'/>
-                </div>
+                <motion.div className="img-wrap" key={doc.id}
+                layout
+                whileHover={{ opacity:1}}
+                onClick= {()=>setSelectedImg(doc.durl)}
+                >
+                    <motion.img src={doc.durl} alt ='uploaded pic'
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
+                    transition={{delay:1}}
+                    />
+                </motion.div>
             ))}
         </div>
     )
