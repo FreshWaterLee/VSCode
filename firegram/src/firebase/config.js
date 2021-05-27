a660cd0a3e6217b17ef30a322ce6a840680cb918
Lee <ihg9303@gmail.com>
2021-05-27 13:13:08 +0900

    'FireGram'

---
+++ firegram/src/firebase/config.js
@@ -0,0 +1,14 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBvwksA_mxIgMbk-9WboGFvXXBaQW-mkyY",
+    authDomain: "reactstudy-b24bb.firebaseapp.com",
+    databaseURL: "https://reactstudy-b24bb-default-rtdb.firebaseio.com",
+    projectId: "reactstudy-b24bb",
+    storageBucket: "reactstudy-b24bb.appspot.com",
+    messagingSenderId: "396496487716",
+    appId: "1:396496487716:web:b5c3249d5ac0fb608d1fb1",
+    measurementId: "G-T8V5B2R9W7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
--- firereac/src/components/dashboard/Notification.js
+++ firereac/src/components/dashboard/Notification.js
@@ -15,7 +15,6 @@ const Notification=(props)=>{
         </div>
       </div>
     </div>
-    
   )
 }
 
