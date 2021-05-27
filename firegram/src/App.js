969785ee4a7d424a6a023d94557d2ca7c8b32411
Lee <ihg9303@gmail.com>
2021-05-27 16:16:42 +0900

    'FireGram0527'

--- firegram/src/App.js
+++ firegram/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
+import ImageGrid from './comps/ImageGrid';
+import Model from './comps/Model';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm'
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title/>
-      <UploadForm />
+      <UploadForm/>
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      {selectedImg && <Model selectedImg = {selectedImg} setSelectedImg={setSelectedImg}/>}
     </div>
   );
 }
--- firegram/src/hooks/useStorage.js
+++ firegram/src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import { projectStorage} from '../firebase/config'
+import { projectStorage, projectFirestore, timestamp} from '../firebase/config'
 
 const useStorage = (file) =>{
     const [progress, setProgress] = useState(0);
@@ -9,6 +9,7 @@ const useStorage = (file) =>{
     useEffect(()=>{
         //references
         const storageRef = projectStorage.ref(file.name);
+        const collectionRef = projectFirestore.collection('images');
         storageRef.put(file).on('state_changed',(snap)=>{
             let percentage = (snap.bytesTransferred/snap.totalBytes)*100;
             setProgress(percentage);
@@ -16,12 +17,13 @@ const useStorage = (file) =>{
             setError(err);
         },async ()=>{
             const durl = await storageRef.getDownloadURL();
+            const createdAt = timestamp();
+            collectionRef.add({durl, createdAt });
             setUrl(durl);
         })
     },[file]);
 
     return{progress, url, error}
-
 }
 
 export default useStorage;
