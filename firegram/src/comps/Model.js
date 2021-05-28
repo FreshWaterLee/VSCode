6c95a06fd086f95f7fac93676f69e00eb6ca258d
Lee <ihg9303@gmail.com>
2021-05-28 09:11:24 +0900

    "expendTracker"

--- firegram/src/comps/Model.js
+++ firegram/src/comps/Model.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const Model=({selectedImg, setSelectedImg})=>{
     const handleClick=(e)=>{
@@ -7,9 +8,15 @@ const Model=({selectedImg, setSelectedImg})=>{
         }
     }
   return(
-    <div className = 'backdrop' onClick={handleClick}>
-      <img className = 'backdrop-img' src={selectedImg} alt = "enlarged pic"/>
-    </div>
+    <motion.div className = 'backdrop' onClick={handleClick}
+        initial={{opacity:0}} // 천천히 나타나는 애니메이션을 위해 초반 투명도
+        animate={{opacity:1}} // 애니메이션 투명도 설정
+    >
+      <motion.img className = 'backdrop-img' src={selectedImg} alt = "enlarged pic"
+        initial={{y:"-100vh" }} // 시작 위치
+        animate = {{y:0}} //애니메이션의 결과 위치
+      />
+    </motion.div>
   )
 }
 
