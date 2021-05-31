9dffa95f276c5db3738897f595bc076fb37bdc3f
Lee <ihg9303@gmail.com>
2021-05-31 15:57:56 +0900

    'AcmePortal_Fail from Network Error'

--- acme_portal/src/components/pages/Staff.js
+++ acme_portal/src/components/pages/Staff.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Staff() {
-    return (
-        <div>
-            <h1>Staff</h1>
-        </div>
-    )
+class Staff extends Component {
+    state = {
+        currentUserName:'Lee',
+        currentUserEmail:'Lee@FreshWater.com'
+    }
+
+    // componentDidMount(){
+    //     const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    //     this.setState({
+    //         currentUserEmail : idToken.idToken.claims.email,
+    //         currentUserEmail : idToken.idToken.claims.email,
+    //     })
+
+    // }
+
+    render() {
+        console.log('Login_Success');
+        return (
+            <div>
+                <h1>Wecome {this.state.currentUserName}</h1>
+                <p>Email: {this.state.currentUserEmail}</p>
+                <p>You have reached the authorized staff area of the portal</p>
+            </div>
+        )
+    }
 }
+
+export default Staff;
