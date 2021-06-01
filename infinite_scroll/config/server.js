cb1cd166bb4d1d1f9cf19fb367dfde9f6309619a
Lee <ihg9303@gmail.com>
2021-06-01 10:55:20 +0900

    'infiniteScroll'

---
+++ infinite_scroll/config/server.js
@@ -0,0 +1,5 @@
+module.exports ={
+    APPLICATION_ID: process.env.APPLICATION_ID || 'd0sxsVD6wE0JzZRky_JiFpIjr6l0z2_w8IJbUMXF0xM',
+    SECRET:process.env.SECRET || 'sqO-NOBzA8abuMeqJe3mB6CCNGMC1_CtQJjClaFJc8k',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
