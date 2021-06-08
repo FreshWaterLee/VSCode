451247a53fab7c8fcf68f5b079939dcede5778fc
Lee <ihg9303@gmail.com>
2021-06-08 09:54:08 +0900

    '0608Rest'

--- LyricSearch/src/context.js
+++ LyricSearch/src/context.js
@@ -11,7 +11,8 @@ export class Provider extends Component {
         ],
         heading:'Top 10 Tracks'
     };
-    // https://cors-anywhere.herokuapp.com/ <- 웹 API 사용시 앞에 작성하면 연결할 수 있다
+    // https://cors-anywhere.herokuapp.com/ 
+    // <- 웹 API 사용시 앞에 작성하면 연결할 수 있다
     componentDidMount(){
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=ko&f_has_lyrics=1&apikey=8bcedbc17a71a0a3b0f55efaa4a6211e`)
         .then(res => {
@@ -20,7 +21,6 @@ export class Provider extends Component {
         })
         .catch(err => console.log(err));
     }
-
     render() {
     return (
         <Context.Provider value ={this.state}>
